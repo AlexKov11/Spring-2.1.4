c173d79b9bd48216a4c32ae0964cd5666918027b
79282 <alexkonov_1@mail.ru>
2024-06-15 16:11:28 +0300

    completed

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Animal animal;
+    private Timer timer;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal2, Timer timer) {
+        this.animal = animal2;
+        this.timer = timer;
+    }
+    public Timer getTimer () {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
